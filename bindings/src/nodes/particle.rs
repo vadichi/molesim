008091c5da08b2c358bd5cce771bf673d4e1b032
Vadim Chichikalyuk <chichikalyuk@gmail.com>
2024-06-04 15:21:29 +0100

    Add scene manager to generate particle mesh

--- bindings/src/nodes/particle.rs
+++ bindings/src/nodes/particle.rs
@@ -1,40 +1,50 @@
 use godot::engine::{ISprite2D, Sprite2D};
 use godot::prelude::*;
 
-use molesim_core::math::vector2::Vector2 as MVector2;
 use molesim_core::entities::particle::MassiveParticle;
+use molesim_core::entities::SimulationEntity;
+use molesim_core::math::vector2::MVector2;
 
 use crate::util::Convert;
 
 #[derive(GodotClass)]
 #[class(base=Sprite2D)]
-struct Particle {
+pub struct Particle {
     base: Base<Sprite2D>,
 
+    is_initialized: bool,
     simulation_entity: MassiveParticle,
 }
 
 #[godot_api]
 impl ISprite2D for Particle {
     fn init(base: Base<Sprite2D>) -> Self {
-        let initial_position = base.as_gd().get_position();
-        
         Self {
             base,
-            simulation_entity: MassiveParticle::new(
-                MVector2::convert(initial_position),
-                MVector2::new(40.0, 10.0),
-            ),
+
+            is_initialized: false,
+            simulation_entity: MassiveParticle::new(MVector2::zero(), MVector2::new(40.0, 10.0)),
         }
     }
 
-    fn physics_process(&mut self, delta: f64) {
-        // self.simulation_entity.update(delta);
-        // let position = self.simulation_entity.position();
+    fn enter_tree(&mut self) {
+        let initial_position = self.base.as_gd().get_position();
+        self.simulation_entity
+            .override_position(MVector2::convert(initial_position));
 
-        godot_print!("Position: {:?}", self.base().get_position());
-        
-        // self.base_mut()
-        //     .set_position(Vector2::new(position.x() as f32, position.y() as f32));
+        self.is_initialized = true;
+    }
+
+    fn physics_process(&mut self, delta: f64) {
+        if !self.is_initialized {
+            return;
+        }
+
+        self.simulation_entity
+            .override_acceleration(MVector2::new(0.0, 9.81));
+        self.simulation_entity.update(delta);
+
+        let position = self.simulation_entity.position();
+        self.base_mut().set_position(Vector2::convert(position));
     }
 }
